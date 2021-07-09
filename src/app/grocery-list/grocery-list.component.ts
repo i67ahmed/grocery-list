df6680288dd6538c4b240d4e825446e09c1a8b32
i67ahmed <i67ahmed@gmail.com>
2021-07-09 14:20:44 -0400

    set up onclick and bootstrap

--- src/app/grocery-list/grocery-list.component.ts
+++ src/app/grocery-list/grocery-list.component.ts
@@ -3,11 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-grocery-list',
   templateUrl: './grocery-list.component.html',
-  styleUrls: ['./grocery-list.component.css']
+  styleUrls: ['../../assets/bootstrap-5.0.2-dist/css/bootstrap.min.css']
 })
 export class GroceryListComponent implements OnInit {
 
-  constructor() { }
+  task = " ";
+  tasks : string[] = [];
+  
+  onClick(){
+    this.tasks.push(this.task);
+    this.task = " ";
+}
 
   ngOnInit(): void {
   }
